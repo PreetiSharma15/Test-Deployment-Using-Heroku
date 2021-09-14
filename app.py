38c601d47b9460f924d7dd5de11c2e9d1b5b6eb9
PreetiSharma15 <pwhitehat6@gmail.com>
2021-09-14 13:53:50 +0530

    Fix app port

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+freeport =   os.getenv('PORT')
+if(not freeport): freeport = 5000
+print('freeport is: ', freeport)
 
 @app.route("/", methods=['GET'])
 def index():
     return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(port=freeport, host='localhost')

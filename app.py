85cc710480333565a7e281631f9c7059114088c9
PreetiSharma15 <pwhitehat6@gmail.com>
2021-09-14 14:09:28 +0530

    without using os freeport

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import os
 
 app = Flask(__name__)
 
-freeport =   os.getenv('PORT')
-if(not freeport): freeport = 5000
-print('freeport is: ', freeport)
+# freeport =   os.getenv('PORT')
+# if(not freeport): freeport = 5000
+# print('freeport is: ', freeport)
 
 @app.route("/", methods=['GET'])
 def index():
     return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(port=freeport, host='localhost')
+# if __name__ == '__main__':
+#     app.run(port=freeport, host='localhost')

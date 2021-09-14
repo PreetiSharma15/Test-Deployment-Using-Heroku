555592eed50f4c75630f0c89351e7e32bcb7107a
PreetiSharma15 <pwhitehat6@gmail.com>
2021-09-14 18:42:58 +0530

    Model File Added

--- app.py
+++ app.py
@@ -3,14 +3,8 @@ import os
 from model_prediction import prediction
 app = Flask(__name__)
 
-# freeport =   os.getenv('PORT')
-# if(not freeport): freeport = 5000
-# print('freeport is: ', freeport)
 
 @app.route("/", methods=['GET'])
 def index():
     prediction_class_label = str(prediction())
     return prediction_class_label
-
-# if __name__ == '__main__':
-#     app.run(port=freeport, host='localhost')
